48dd925b1a90ea9382100e69dcd36e02c619467a
Gaffer Fitch <gaffer.fitch@gmail.com>
2019-04-13 14:39:47 -0400

    Updated Install with Download and Speech

--- www/js/service.js
+++ www/js/service.js
@@ -10,12 +10,12 @@ DRApp.partial("Footer",DRApp.load("footer"));
 
 DRApp.template("Home",DRApp.load("home"),null,DRApp.partials);
 DRApp.template("Install",DRApp.load("install"),null,DRApp.partials);
-DRApp.template("Dcoumentation",DRApp.load("documentation"),null,DRApp.partials);
+DRApp.template("Apps",DRApp.load("apps"),null,DRApp.partials);
 DRApp.template("Architecture",DRApp.load("architecture"),null,DRApp.partials);
 DRApp.template("Development",DRApp.load("development"),null,DRApp.partials);
 
 DRApp.route("home","/","Home");
 DRApp.route("install","/install","Install");
-DRApp.route("apps","/documentation","Apps");
+DRApp.route("apps","/apps","Apps");
 DRApp.route("architecture","/architecture","Architecture");
 DRApp.route("development","/development","Development");
